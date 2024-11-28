1e419e3751a331415fe1dd5116e58f0196de29b2
JacksonBaer <jackson@baerforce.net>
2024-11-27 23:18:01 -0600

    Update autostartlxde.sh

--- autostartlxde.sh
+++ autostartlxde.sh
@@ -27,28 +27,3 @@ EOL
 chmod +x ~/thinclient
 #Restarting the client
 sudo reboot
-
-
-# Add the required line to the user's autostart file
-echo "@/usr/bin/bash /home/vdiuser/thinclient" > ~/.config/lxsession/LXDE/autostart
-
-# Create thin client script
-echo "Creating thinclient script..."
-touch /home/vdiuser/thinclient
-
-cat <<'EOL' > /home/vdiuser/thinclient
-#!/bin/bash
-sleep 1
-/usr/bin/openbox --exit
-# Navigate to the PVE-VDIClient directory
-cd ~/PVE-VDIClient
-# Run loop for thin client to prevent user closure
-while true; do
-    /usr/bin/python3 ~/PVE-VDIClient/vdiclient.py
-done
-EOL
-
-# Make thinclient script executable
-chmod +x ~/thinclient
-#Restarting the client
-sudo reboot
